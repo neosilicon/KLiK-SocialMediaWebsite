2c1aa00ac44dc69e35fbd11cfc92ceeb51bc55fe
Luis Gonzalez <lgonzal@redhat.com>
2021-03-23 03:13:19 -0400

    update

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,7 +5,7 @@ $dBUsername = $_ENV["DATABASE_USER"]; //root
 $dBPassword = $_ENV["DATABASE_PASSWORD"]; //"eldererajinMenji99";
 $dBName = "klik_database"; //klik_database
 
-die("DB SVC NAME". $_ENV["DATABASE_SERVICE_NAME"]);
+echo("DB SVC NAME". $_ENV["DATABASE_SERVICE_NAME"]);
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName, 3306);
 
